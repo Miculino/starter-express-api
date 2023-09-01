0e855c21a89e79b8e74848dad0cf0302c2cf4586
Miculino <128690939+Miculino@users.noreply.github.com>
2023-09-01 11:28:37 +0200

    added api endpoint

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 const express = require('express')
+const puppeteer = require('puppeteer')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('test.ejs')
 })
-app.listen(process.env.PORT || 3000)
+
+
+
+app.post('/api/snapshot', async (req, res) => {
+    // if (!req.query.url || !req.query.width || !req.query.height) {
+    //     res.status(400).send({message: 'Invalid input'})
+    //     return
+    // }
+  const { finalCode } = req.body;
+  console.log(req.body)
+
+    // Create a snapshot and send it back
+    const browser = await puppeteer.launch({ headless: 'new', defaultViewport: {
+      width: 1920,
+      height: 1080
+    }})
+    const page = await browser.newPage()
+    await page.setContent(finalCode)
+    // await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36')
+    // await page.goto('http://localhost:3800', {waitUntil: 'networkidle2'})
+    const buffer =   await page.screenshot({
+      path: 'screenshot_full.png',
+      fullPage: true
+  })
+  console.log(buffer);
+    // Send it back
+    res.setHeader('content-type', 'image/png')
+    res.write(buffer,'binary')
+    res.end(null, 'binary')
+    // Close browser and page
+    await page.close()
+  })
+
+app.listen(process.env.PORT || 3800)

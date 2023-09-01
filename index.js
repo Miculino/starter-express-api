c39a79e6d504e486e920b955fda3899611a32d10
Miculino <128690939+Miculino@users.noreply.github.com>
2023-09-01 11:51:00 +0200

    add cors config

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const bodyParser = require('body-parser');
 
 const app = express()
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}));
 
 app.use(bodyParser.urlencoded({
   extended: true
